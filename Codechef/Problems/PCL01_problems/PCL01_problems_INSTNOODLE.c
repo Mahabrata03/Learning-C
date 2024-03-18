39f2e336a08a1376ed0911bff5861a22c36fbfb9
Mahabrata03 <150134956+Mahabrata03@users.noreply.github.com>
2024-03-18 23:39:14 +0530

    c leqarn

---
+++ Codechef/Problems/PCL01_problems/PCL01_problems_INSTNOODLE.c
@@ -0,0 +1,21 @@
+// Input Format
+// The first and only line of input contains two space-separated integers 
+// �
+// X and 
+// �
+// Y — the number of stoves and the number of minutes, respectively.
+// Output Format
+// Print a single integer, the maximum number of customers Chef can serve in 
+// �
+// Y minutes
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x,y;
+	scanf("%d %d",&x ,&y);
+	printf("%d",x*y	);
+	
+
+}
