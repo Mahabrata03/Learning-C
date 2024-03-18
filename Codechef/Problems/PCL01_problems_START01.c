fd97f4e5edbc96171a71d648f1d5c4442d59ccbd
Mahabrata03 <150134956+Mahabrata03@users.noreply.github.com>
2024-03-18 23:25:43 +0530

    c learn

---
+++ Codechef/Problems/PCL01_problems_START01.c
@@ -0,0 +1,14 @@
+// Input Format
+// The only line of input contains a single integer.
+
+// Output Format
+// Output the answer in a single line.
+
+#include <stdio.h>
+
+int main(void) {
+  int a;
+  scanf("%d",&a);
+  printf("%d",a);
+  return 0;
+  
---
+++ Codechef/Problems/Q1.c
@@ -0,0 +1,5 @@
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.
